6a6675b4892ffdd35c4dca59e420285b6baba32b
Paul Madden <paul.a.madden@noaa.gov>
2013-01-15 12:31:32 -0700

    Add Procs & Nodes columns to output

--- qmon.clj
+++ qmon.clj
@@ -9,9 +9,9 @@
   (:use [clojure.xml :only [parse]])
   (:use [clojure.pprint]))
 
-(def fmtstr "%-8s %-16s %-8s %-8s %-1s %-8s %-9s %s")
+(def fmtstr "%-8s %-16s %-8s %-8s %-5s %-5s %-1s %-8s %-9s %s")
 
-(declare tags tree xml xtime)
+(declare tags tree xml xprocs xres xtime)
 
 (defn contents [job]  (map #(:content %) job))
 (defn d        [n]    (apply str (repeat n "-")))
@@ -19,17 +19,23 @@
 (defn jobs     []     (map :content (filter #(= (:tag %) :Job) (tree))))
 (defn re       [user] (re-pattern (str user "@.*")))
 (defn tags     [job]  (map #(:tag %) job))
-(defn tree     []     (:content (parse (java.io.StringBufferInputStream. (xml)))))
 (defn xattr    [j,k]  (first (k j)))
 (defn xjobid   [j]    (xattr j :Job_Id))
 (defn xml      []     (:out (sh "qstat" "-x")))
 (defn xname    [j]    (xattr j :Job_Name))
+(defn xnodes   [j]    (if (= "-" (xprocs j)) (xres j :nodect) "-"))
 (defn xowner   [j]    (xattr j :Job_Owner))
 (defn xqueue   [j]    (xattr j :queue))
+(defn xprocs   [j]    (let [procs (xres j :procs) ] (if (nil? procs) "-" procs)))
+(defn xres     [j,k]  (first (:content (first (filter #(= (:tag %) k) (:Resource_List j))))))
 (defn xrtime   [j]    (xtime j :Resource_List))
 (defn xstate   [j]    (xattr j :job_state))
 (defn xtime    [j,k]  (first (:content (first (filter #(= (:tag %) :walltime) (k j))))))
 (defn xutime   [j]    (let [t (xtime j :resources_used)] (if (nil? t) "-" t)))
+
+(defn tree []
+  (try (:content (parse (java.io.StringBufferInputStream. (xml))))
+       (catch Exception e)))
 
 (defn xpart [j]
   (let [p (first (:content (first (filter #(= (:tag %) :partition) (:Resource_List j)))))]
@@ -39,8 +45,10 @@
   (let [j (jobraw job)]
     {:jobid (xjobid j)
      :name  (xname  j)
+     :nodes (xnodes j)
      :owner (xowner j)
-     :part  (xpart j)
+     :part  (xpart  j)
+     :procs (xprocs j)
      :queue (xqueue j)
      :rtime (xrtime j)
      :state (xstate j)
@@ -66,6 +74,8 @@
           (re-find (re-matcher #"^[^@]+" (:owner j)))
           (:rtime j)
           (:utime j)
+          (:procs j)
+          (:nodes j)
           (:state j)
           (:queue j)
           (:part j)
@@ -73,8 +83,8 @@
 
 (defn prhead [msg]
   (str (format "\n%s\n\n" msg)
-       (format fmtstr "Job ID" "Owner" "Req'd" "Used" "S" "Queue" "Partition" "Job Name\n")
-       (format fmtstr (d 8) (d 16) (d 8) (d 8) (d 1) (d 8) (d 9) (d 32))
+       (format fmtstr "Job ID" "Owner" "Req'd" "Used" "Procs" "Nodes" "S" "Queue" "Partition" "Job Name\n")
+       (format fmtstr (d 8) (d 16) (d 8) (d 8) (d 5) (d 5) (d 1) (d 8) (d 9) (d 32))
        "\n"))
 
 (defn prjobs [key,msg,s]
@@ -95,7 +105,7 @@
   (do
     (.setFont ta (Font. "Monospaced" (Font/PLAIN) 12))
     (doto fr
-      (.setSize 725 600)
+      (.setSize 800 600)
       (.setLayout (BorderLayout.))
       (.add ta BorderLayout/CENTER)
       (.setDefaultCloseOperation JFrame/EXIT_ON_CLOSE)

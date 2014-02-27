4cad13279e2c46077e6a580d0ba83b3ce745561a
Paul Madden <maddenp@colorado.edu>
2014-02-27 10:42:12 -0700

    dynamic column widths & general refactoring

--- qmon.clj
+++ qmon.clj
@@ -6,101 +6,109 @@
            [java.io StringBufferInputStream]
            [javax.swing JFrame])
   (:use [clojure.java.shell :only [sh]])
-  (:use [clojure.xml :only [parse]])
-  (:use [clojure.pprint]))
+  (:use [clojure.string :only [join]])
+  (:use [clojure.xml :only [parse]]))
 
-(def fmtstr "%-8s %-16s %-8s %-8s %-5s %-5s %-1s %-8s %-9s %s")
+(def headkeys [:jobid :owner :rtime :utime :procs :nodes :state :queue :part :name])
+(def headvals ["Job ID" "Owner" "Requested" "Used" "Procs" "Nodes" "S" "Queue" "Partition" "Job Name"])
 
-(declare tags tree xml xprocs xres xtime)
+(declare split tree xprocs xres xtime)
 
-(defn contents [job]  (map #(:content %) job))
-(defn d        [n]    (apply str (repeat n "-")))
-(defn jobraw   [job]  (zipmap (tags job) (contents job)))
-(defn jobs     []     (map :content (filter #(= (:tag %) :Job) (tree))))
-(defn re       [user] (re-pattern (str user "@.*")))
-(defn tags     [job]  (map #(:tag %) job))
-(defn xattr    [j,k]  (first (k j)))
-(defn xjobid   [j]    (xattr j :Job_Id))
-(defn xml      []     (:out (sh "qstat" "-x")))
-(defn xname    [j]    (xattr j :Job_Name))
-(defn xnodes   [j]    (if (= "-" (xprocs j)) (xres j :nodect) "-"))
-(defn xowner   [j]    (xattr j :Job_Owner))
-(defn xqueue   [j]    (xattr j :queue))
-(defn xprocs   [j]    (let [procs (xres j :procs) ] (if (nil? procs) "-" procs)))
-(defn xres     [j,k]  (first (:content (first (filter #(= (:tag %) k) (:Resource_List j))))))
-(defn xrtime   [j]    (xtime j :Resource_List))
-(defn xstate   [j]    (xattr j :job_state))
-(defn xtime    [j,k]  (first (:content (first (filter #(= (:tag %) :walltime) (k j))))))
-(defn xutime   [j]    (let [t (xtime j :resources_used)] (if (nil? t) "-" t)))
+(defn colwidths [jobs]
+  (let [headwidth (map #(count ((zipmap headkeys headvals) %)) headkeys)
+        datawidth (map #(reduce max (map count (map % jobs))) headkeys)]
+    (map max headwidth datawidth)))
+
+(defn jobinfo [job]
+  (let [fns (map #(ns-resolve *ns* (symbol (str "x" (name %)))) headkeys)
+        raw (fn [job] (zipmap (map #(:tag %) job) (map #(:content %) job)))]
+    (zipmap headkeys (map #(% (raw job)) fns))))
+
+(defn myname []
+  (if (first *command-line-args*)
+    (first *command-line-args*)
+    (get (System/getenv) "USER")))
+
+(defn myjobs [user]
+  (let [alljobs (map :content (filter #(= (:tag %) :Job) (tree)))
+        re (fn [user] (re-pattern (str user "@.*")))
+        x (filter #(re-matches (re user) (:owner %)) (map #(jobinfo %) alljobs))]
+    (map #(assoc %
+            :jobid (re-find (re-matcher #"^\d+" (:jobid %)))
+            :owner (re-find (re-matcher #"^[^@]+" (:owner %))))
+         x)))
+
+(defn prhead [msg fmt sep]
+  (str (format "\n%s\n\n" msg) (apply format fmt headvals) "\n" sep "\n"))
+
+(defn prjobs [fmt sep jobs msg]
+  (let [fmtjob (fn [j fmt] (apply format fmt (map #(% j) headkeys)))]
+    (if (empty? jobs)
+      nil
+      (str (prhead msg fmt sep) (reduce #(str %1 %2) (map #(str (fmtjob % fmt) "\n") jobs))))))
+
+(defn show [user]
+  (let [jobs      (myjobs   user)
+        sections  (split    jobs)
+        colwidths (colwidths jobs)
+        fmt       (join " " (map #(str "%-" % "s") colwidths))
+        dashes    (fn [n] (apply str (repeat n "-")))
+        sep       (join " " (map #(dashes %) colwidths))]
+    (str (format "\n%s\n" (.toString (java.util.Date.)))
+         (prjobs fmt sep (:Q sections) "QUEUED"   )
+         (prjobs fmt sep (:R sections) "RUNNING"  )
+         (prjobs fmt sep (:C sections) "COMPLETED"))))
+
+(defn split [jobs]
+  (let [f (fn [s j] (filter #(= s (:state %)) j))]
+    (apply merge (map #(hash-map % (f (name %) jobs)) [:Q :R :C]))))
 
 (defn tree []
-  (try (:content (parse (java.io.StringBufferInputStream. (xml))))
+  (try (:content (parse (java.io.StringBufferInputStream. (:out (sh "qstat" "-x")))))
        (catch Exception e)))
+
+(defn xattr [j,k]
+  (first (k j)))
+
+(defn xjobid [j]
+  (xattr j :Job_Id))
+
+(defn xname [j]
+  (xattr j :Job_Name))
+
+(defn xnodes [j]
+  (if (= "-" (xprocs j)) (xres j :nodect) "-"))
+
+(defn xowner [j]
+  (xattr j :Job_Owner))
 
 (defn xpart [j]
   (let [p (first (:content (first (filter #(= (:tag %) :partition) (:Resource_List j)))))]
     (if (nil? p) "-" p)))
 
-(defn jobinfo [job]
-  (let [j (jobraw job)]
-    {:jobid (xjobid j)
-     :name  (xname  j)
-     :nodes (xnodes j)
-     :owner (xowner j)
-     :part  (xpart  j)
-     :procs (xprocs j)
-     :queue (xqueue j)
-     :rtime (xrtime j)
-     :state (xstate j)
-     :utime (xutime j)}))
+(defn xprocs [j]
+  (let [procs (xres j :procs) ] (if (nil? procs) "-" procs)))
 
-(defn user []
-  (if (first *command-line-args*)
-    (first *command-line-args*)
-    (get (System/getenv) "USER")))
+(defn xqueue [j]
+  (xattr j :queue))
 
-(defn mine [u]
-  (filter #(re-matches (re u) (:owner %)) (map #(jobinfo %) (jobs))))
+(defn xres [j,k]
+  (first (:content (first (filter #(= (:tag %) k) (:Resource_List j))))))
 
-(defn split [u]
-  (let [m (mine u)]
-    {:Q (filter #(= "Q" (:state %)) m)
-     :R (filter #(= "R" (:state %)) m)
-     :C (filter #(= "C" (:state %)) m)}))
+(defn xrtime [j]
+  (xtime j :Resource_List))
 
-(defn prjob [j]
-  (format fmtstr 
-          (re-find (re-matcher #"^\d+" (:jobid j)))
-          (re-find (re-matcher #"^[^@]+" (:owner j)))
-          (:rtime j)
-          (:utime j)
-          (:procs j)
-          (:nodes j)
-          (:state j)
-          (:queue j)
-          (:part j)
-          (:name  j)))
+(defn xstate [j]
+  (xattr j :job_state))
 
-(defn prhead [msg]
-  (str (format "\n%s\n\n" msg)
-       (format fmtstr "Job ID" "Owner" "Req'd" "Used" "Procs" "Nodes" "S" "Queue" "Partition" "Job Name\n")
-       (format fmtstr (d 8) (d 16) (d 8) (d 8) (d 5) (d 5) (d 1) (d 8) (d 9) (d 32))
-       "\n"))
+(defn xtime [j,k]
+  (first (:content (first (filter #(= (:tag %) :walltime) (k j))))))
 
-(defn prjobs [key,msg,s]
-  (let [jobs (key s)]
-    (if (empty? jobs)
-      nil
-      (str (prhead msg) (reduce #(str %1 %2) (map #(str (prjob %) "\n") (key s)))))))
+(defn xutime [j]
+  (let [t (xtime j :resources_used)] (if (nil? t) "-" t)))
 
-(defn show [u]
-  (let [s (split u)]
-    (str (format "\n%s\n" (.toString (java.util.Date.)))
-         (prjobs :Q "QUEUED" s)
-         (prjobs :R "RUNNING" s)
-         (prjobs :C "COMPLETED" s))))
-
-(let [fr (JFrame. (str "qmon " (user)))
+(let [user (myname)
+      fr (JFrame. (str "qmon " user))
       ta (TextArea. "\nLoading..." )]
   (do
     (.setFont ta (Font. "Monospaced" (Font/PLAIN) 12))
@@ -113,5 +121,5 @@
     (doto ta
       (.setEditable false))
     (while true
-      (.setText ta (show (user)))
+      (.setText ta (show user))
       (Thread/sleep 5000))))
